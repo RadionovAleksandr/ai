75fe2bd209430b488ff0768e5cc437a28edc65b1
aradionov <aradionov@at-consulting.ru>
2021-03-06 00:30:38 +0700

    create pages, components

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,7 +8,7 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display main message', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('ai app is running!');
   });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { MainComponent } from './pages/main/main.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { EditBookComponent } from './pages/edit-book/edit-book.component';
+import { CreateBookComponent } from './pages/create-book/create-book.component';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    BookListComponent,
+    MenuComponent,
+    EditBookComponent,
+    CreateBookComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
